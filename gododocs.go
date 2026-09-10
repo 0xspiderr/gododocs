098357bd71433348075691d94a1fbb290a681ab9
0xspiderr <harypatrascu.1@gmail.com>
2026-09-10 23:42:55 +0300

    first commit

---
+++ gododocs.go
@@ -0,0 +1,12 @@
+package main
+
+// PLAN
+// 1. read input xml file(s) from a given directory sequentially
+// 2. convert xml files of godot documentation to markdown
+// 2.1 -> implies I have to map the generate xml to a markdown structure
+// 2.2 -> optionally expand the tool to convert to other formats
+// 3. output to a given directory from cmdline
+
+func main() {
+
+}
